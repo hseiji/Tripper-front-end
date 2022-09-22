0d06da7768233c98d27327182a00638b76e72f4e
hseiji <henrique.hst@gmail.com>
2022-09-22 08:52:53 -0400

    map config

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,21 +4,24 @@ import { MapLayer } from '../MapLayer/MapLayer';
 
 export const Map = (props) => {
 
-  // Plan based on user_id
-  const plan = {
+  // Using one layer
+  const map = {
     id: 1,
   }
+
+  // Center map
+  const centerInfo = [ 43.6532976025993, -79.38359538925825]
 
   console.log("rendering map");
 
   return (
-    <MapContainer center={[props.user.lat, props.user.lng]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={centerInfo} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <MapLayer key={plan.id} />
+      <MapLayer key={map.id} />
 
 
     </MapContainer>
